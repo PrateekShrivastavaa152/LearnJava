d8552870e3800dfa5b9b9e63573b6e5c81625ac4
PrateeShrivastavaa152 <prateekshrivastava152@gmail.com>
2022-06-23 13:27:28 +0530

    Second commit

--- FirstProject/src/demo/Welcome.java
+++ FirstProject/src/demo/Welcome.java
@@ -3,6 +3,7 @@ package demo;
 public class Welcome {
 	public static void main(String[] args) {
 				System.out.println("Hello World! Welcome to Java Programming");
+				System.out.println("if you know then you know");
 		}   
 
 
