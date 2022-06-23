0115d21d02da84ee1bd0f5720e43b36757c65710
PrateeShrivastavaa152 <prateekshrivastava152@gmail.com>
2022-06-24 00:18:01 +0530

    Third Commit

--- FirstProject/src/demo/Welcome.java
+++ FirstProject/src/demo/Welcome.java
@@ -2,8 +2,12 @@ package demo;
 
 public class Welcome {
 	public static void main(String[] args) {
+		int age = 25;
+		System.out.println(age);
 				System.out.println("Hello World! Welcome to Java Programming");
 				System.out.println("if you know then you know");
+				
+				
 		}   
 
 
